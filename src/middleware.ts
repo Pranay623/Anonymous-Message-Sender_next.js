20a1436040455639150f0dd109896b3dea976c84
Pranay623 <srivastavapranay623@gmail.com>
2025-03-25 19:18:44 +0530

    updated middleware with not token conditions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { getToken } from 'next-auth/jwt'
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
 export {default} from 'next-auth/middleware'
 export {getToken} from 'next-auth/jwt'
  
@@ -10,16 +10,22 @@ export async function middleware(request: NextRequest) {
     const token = await getToken({req: request})
     const url = request.nextUrl
 
-    if(token && (
+    {if(token && (
         url.pathname.startsWith('/sign-in') ||
         url.pathname.startsWith('/sign-up') ||
         url.pathname.startsWith('/verify') ||
         url.pathname.startsWith('/') 
     ))
 
+  return NextResponse.redirect(new URL('/dashboard', request.url))}
 
-  return NextResponse.redirect(new URL('/home', request.url))
+  if(!token && url.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+  return NextResponse.next();
 }
+
+
  
 // See "Matching Paths" below to learn more
 export const config = {
